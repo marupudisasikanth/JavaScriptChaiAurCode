a6f55972ecd7f95b3f8be75ac2538bf02a1a62bf
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-24 15:36:32 +0000

    at video 8

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log("02">= 2)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null===0)
+console.log(null>=0)
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined===0)
+console.log(undefined>=0)
+
+console.log(undefined==null)
+console.log(null==undefined)
+
+console.log("2">false)
+console.log(true>false)
+
+
+

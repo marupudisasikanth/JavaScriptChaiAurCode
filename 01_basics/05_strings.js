9fb060bdf6929471b3dc434cf21d2402653b094b
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-24 20:06:29 +0000

    at video 11

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+let s = "sasikanth"
+
+// console.log(s.at(5));
+// console.log(s.anchor());
+// console.log(s.big());
+// console.log(s.blink());
+
+
+let str = "JavaScript";
+// console.log(str.slice(2, -2) );
+
+let myName = 'hitesh'
+// console.log(myName.substring(0,4))
+// console.log(myName.substring(5,4))
+// console.log(myName.slice(3,4))
+console.log(myName.slice(-2,4))
+

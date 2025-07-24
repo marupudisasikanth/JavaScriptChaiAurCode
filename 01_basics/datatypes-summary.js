ea382ef13c4641d3a7c4d0ee84cd3e9e69a61875
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-24 16:55:34 +0000

    at video 10

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -15,5 +15,24 @@
 // 3 Functions
 let bigNumber = 321586947258475225482452n;
 console.log(typeof bigNumber);
-//
-https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Types of memory in js
+// Stack (primitive), Heap (Non-primitive)
+
+let myName = "sasikanth"
+
+let otherName = myName
+myName='sas'
+console.log(otherName)
+
+let user = {
+    hName: 'spike'
+}
+
+let user2 = user
+user.hName='chan'
+
+console.log(user2)

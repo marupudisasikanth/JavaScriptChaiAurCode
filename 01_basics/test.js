ddeff02014172689302cba6df8ad94ecf3199d24
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-24 14:16:43 +0000

    at video 6

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
 console.log("sas");
+
+let name = 'sas'
+let age = 30
+
+console.table([name,age])
+
+let isLoggedIn =0
+console.log(new Boolean(isLoggedIn))
+let names ='saf'
+console.log(!!names)
+console.log(Boolean(names))
+
+let num = Number('30')
+console.log(num)
+
+let number = 50
+console.log(typeof new String(number))
+console.log(typeof new Boolean(5))

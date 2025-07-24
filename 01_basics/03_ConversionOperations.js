c9b70f3848e67361827bea4ef11da3c791a594fe
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-24 15:20:19 +0000

    at video 7

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,12 @@
+let value = 3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+
+console.log("1"+2)
+console.log(2+1/"2")
+console.log((3+4)*5%3)
+console.log(+true+20)
+

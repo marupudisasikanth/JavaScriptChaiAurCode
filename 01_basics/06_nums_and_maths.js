f49f69c4d75e079dfb7d45437132670452bcd4e9
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-25 07:06:19 +0000

    at video 12

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score =400
+
+let balance = new Number(100)
+let otherBalance = new Number(100)
+console.log(balance.valueOf()==otherBalance.valueOf());
+balance=20
+
+console.log(score)
+console.log(Number(101.00012258).toFixed(12));
+console.log(101.2536849.toFixed(2));
+console.log(101.2356.toPrecision(5));
+console.log(101.2356.toFixed(2));
+
+const hundereds =1000000000000
+console.log(hundereds.toLocaleString('en-IN'));
+console.log(hundereds.toLocaleString());
+
+// Math
+
+console.log(Math.PI);
+console.log(Math.pow(4,2));
+console.log(Math.sqrt(16));
+
+let arr = [4,3,6,8,9,20]
+console.log(Math.min(...arr));
+
+
+
+
+
+
+
+

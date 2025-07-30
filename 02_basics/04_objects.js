ccbb5110603334eda6e7ba925a341afffa1aeda4
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-30 19:17:31 +0000

    at video 17

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,77 @@
+
+
+const createSingleton = (()=>{
+    let instance;
+    function createInstance(par){
+        return par;
+    }
+    return{
+        getInstance: (param)=>{
+            if(!instance){
+            instance =  createInstance(param)
+        }
+        return instance;
+        }
+        
+    };
+})();
+console.log(createSingleton.getInstance('sak')==createSingleton.getInstance('sak'));
+
+
+
+// SUMMARY : 
+
+// 1.To declare singleton object => new Object( );
+// 2. To merge object use spread operartor. {...Obj1, ...Obj2 } or 
+// Object.assign( { }, obj1,obj2 ):
+// 3. To return keys from object => Object.keys(object name);
+// 4.To return values from object => Object.values(object name);
+// 5.To check property of object => Object.hasOwnProperty(key/value name);
+const tindrUser = new Object()
+const proto = {
+  greet() {
+    console.log("Hello");
+  }
+};
+const person = Object.create(proto);
+person.name = "John";
+
+person.greet()
+tindrUser.id="123abc"
+tindrUser.name = "Sammy"
+console.log(tindrUser);
+
+const obj1 ={ name :'sas', age:30}
+const obj2 ={ city: 'HYD'}
+const obj3 ={obj1,obj2}
+console.log(obj3);
+
+//{} optional
+const obj4 = Object.assign({},obj1,obj2)
+console.log(obj4);
+
+const obj5 ={...obj1, ...obj2}
+
+console.log(obj5);
+
+console.log(Object.keys(obj5));
+console.log(Object.values(obj5));
+console.log(Object.entries(obj5));
+
+const flattened = Object.entries(obj5).flat(1)
+console.log(flattened);
+
+const obj6 =new Object({...obj1, ...obj2})
+console.log(obj6.hasOwnProperty('name'));
+tindrUser.hasOwnProperty()
+
+
+
+
+
+
+
+
+
+
+

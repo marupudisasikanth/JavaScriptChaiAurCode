1f4df23211e3e2d9dd42ea6218481e98dac5c8e3
marupudisasikanth <marupudisasikanth@gmail.com>
2025-07-31 15:28:48 +0000

    at video 24

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// https://www.youtube.com/watch?v=GAIbn16Iytc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=24
+
+(function chai(){
+    console.log(`DB Connected`);
+})();
+(function chai(){
+    console.log(`DB Connected`);
+})();
+
+(()=>{
+    console.log(`DB Connected`);
+})();
+
+((name)=>{
+    console.log(`DB Connected ${name}`);
+})('sasi');
